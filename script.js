6e4c24d454a1a2a1156d38f5f0df8fc97a72d963
fmcdonnell <finnmcdonnell99@gmail.com>
2020-08-17 16:53:20 +0100

    added some js getelemntbyID

---
+++ script.js
@@ -0,0 +1,25 @@
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+let zero = document.getElementById("zero");
+let dot = document.getElementById("dot");
+let plus = document.getElementById("plus");
+let minus = document.getElementById("minus");
+let divide = document.getElementById("divide");
+let multiply = document.getElementById("multiply");
+let backspace = document.getElementById("backspace");
+let equal = document.getElementById("equal");
+let pi = document.getElementById("pi");
+let e = document.getElementById("e");
+let sqrt = document.getElementById("sqrt");
+let power = document.getElementById("power");
+let clear = document.getElementById("clear");
+let result = document.getElementById("result");
+
+
